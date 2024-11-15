606c3981825040c24257424f9705b29fc94ba76f
John Pham <johnpham456@gmail.com>
2024-11-14 22:00:53 -0500

    base game + basic challenges

--- MathGame/GameEngine.cs
+++ MathGame/GameEngine.cs
@@ -1,6 +1,72 @@
+using System.Diagnostics;
+using MathGame.Models;
+
 namespace MathGame;
 
-public class GameEngine
+internal class GameEngine
 {
-    
+ internal void Game(GameType gameType, Difficulty difficulty)
+ {
+  var score = 0;
+  var random = new Random();
+  bool randomGame = gameType == GameType.Random;
+
+  var stopwatch = Stopwatch.StartNew();
+  for (int i = 0; i < 5; i++)
+  {
+   if (randomGame)
+   {
+    gameType = (GameType)random.Next(0, 4);
+   }
+   
+   var numbers = Helpers.GetNumbers(gameType, difficulty);
+   var value1 = numbers[0];
+   var value2 = numbers[1];
+   
+   var operatorString = gameType switch
+   {
+    GameType.Addition => "+",
+    GameType.Subtraction => "-",
+    GameType.Multiplication => "*",
+    GameType.Division => "/",
+    _ => throw new ArgumentException($"Invalid game type {gameType}.")
+   };
+
+   var correctAnswer = gameType switch
+   {
+    GameType.Addition => value1 + value2,
+    GameType.Subtraction => value1 - value2,
+    GameType.Multiplication => value1 * value2,
+    GameType.Division => value1 / value2,
+    _ => throw new ArgumentException($"Invalid game type {gameType}.")
+   };
+   
+   Console.Clear();
+   Console.WriteLine($"{value1} {operatorString} {value2} = ");
+   
+   var input = Console.ReadLine();
+   input = Helpers.ValidateInput(input);
+   if (int.Parse(input) == correctAnswer)
+   {
+    Console.WriteLine("Correct! Press any key for the next question.");
+    score++;
+   }
+   else
+   {
+    Console.WriteLine($"Incorrect. The correct answer was {correctAnswer}. Press any key for the next question.");
+   }
+   
+   if (i == 4)
+   {
+    stopwatch.Stop();
+   }
+   
+   Console.ReadKey();
+  }
+  Console.Clear();
+  Console.WriteLine($"Game over. Your final score is {score}. This took {stopwatch.Elapsed.TotalSeconds} seconds.");
+  Console.WriteLine("Press any key to return to the main menu.");
+  Console.ReadKey();
+  Helpers.AddToHistory(score, randomGame ? GameType.Random : gameType, difficulty, stopwatch.Elapsed.TotalSeconds);
+ }
 }
--- MathGame/Menu.cs
+++ MathGame/Menu.cs
@@ -1,6 +1,96 @@
+using MathGame.Models;
+
 namespace MathGame;
 
-public class Menu
+internal class Menu
 {
-    
+    private GameEngine gameClass = new();
+
+    internal void ShowMenu(string name, DateTime date)
+    {
+        Console.Clear();
+        Console.WriteLine($"Hello {name}. It's {date}. This is a math game.");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        Console.WriteLine("\n");
+
+        var gameRunning = true;
+
+        while (gameRunning)
+        {
+            Console.Clear();
+            Console.WriteLine(@$"What game would you like to play today? Choose from the option below:
+V - View Previous Games
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+R - Random
+Q - Quit the program");
+            Console.WriteLine("---------------------------------------------");
+
+            var gameSelected = Console.ReadLine();
+
+            switch (gameSelected.Trim().ToLower())
+            {
+                case "v":
+                    Helpers.PrintGames();
+                    break;
+                case "a":
+                    ChooseDifficulty(GameType.Addition);
+                    break;
+                case "s":
+                    ChooseDifficulty(GameType.Subtraction);
+                    break;
+                case "m":
+                    ChooseDifficulty(GameType.Multiplication);
+                    break;
+                case "d":
+                    ChooseDifficulty(GameType.Division);
+                    break;
+                case "r":
+                    ChooseDifficulty(GameType.Random);
+                    break;
+                case "q":
+                    Console.WriteLine("Goodbye!");
+                    gameRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Please choose a valid option");
+                    break;
+            }
+        }
+        
+    }
+
+    internal void ChooseDifficulty(GameType gameType)
+    {
+        Console.Clear();
+        Console.WriteLine($"""
+                           Choose a difficulty:
+                           E - Easy
+                           N - Normal
+                           H - Hard
+                           """);
+        Console.WriteLine("---------------------------------------------");
+        
+        var input = Console.ReadLine();
+        input = input.Trim().ToLower();
+
+        while (string.IsNullOrEmpty(input) || (input != "e" && input != "n" && input != "h"))
+        {
+            Console.WriteLine("Please choose a valid difficulty");
+            input = Console.ReadLine();
+        }
+
+        Difficulty difficulty =  input switch
+        {
+            "e" => Difficulty.Easy,
+            "n" => Difficulty.Normal,
+            "h" => Difficulty.Hard,
+            _ => throw new ArgumentException("Invalid difficulty.")
+        };
+        
+        gameClass.Game(gameType, difficulty);
+    }
 }
